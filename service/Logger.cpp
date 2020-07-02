dce6fd27412c23a655b780d81e8cfce1c219d513
adamy <adamy@git.code.sf.net>
2020-07-02 17:34:15 +0100

    1.6.6, initial release

--- service/Logger.cpp
+++ service/Logger.cpp
@@ -1,5 +1,5 @@
 #include <edidentifier.h>
-__CIDENT_RCSID(Logger_cpp, "$Id: Logger.cpp,v 1.4 2020/07/02 01:51:22 cvsuser Exp $")
+__CIDENT_RCSID(Logger_cpp, "$Id: Logger.cpp,v 1.5 2020/07/02 16:25:21 cvsuser Exp $")
 
 /* -*- mode: c; indent-width: 4; -*- */
 /*
@@ -8,12 +8,9 @@ __CIDENT_RCSID(Logger_cpp, "$Id: Logger.cpp,v 1.4 2020/07/02 01:51:22 cvsuser Ex
  * Copyright (c) 2020, Adam Young.
  * All rights reserved.
  *
- * This file is part of memcached-win32.
- *
  * The applications are free software: you can redistribute it
  * and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
+ * published by the Free Software Foundation, version 3.
  *
  * Redistributions of source code must retain the above copyright
  * notice, and must be distributed with the license document above.
@@ -26,6 +23,7 @@ __CIDENT_RCSID(Logger_cpp, "$Id: Logger.cpp,v 1.4 2020/07/02 01:51:22 cvsuser Ex
  * This project is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * license for more details.
  * ==end==
  */
 
--- service/Logger.h
+++ service/Logger.h
@@ -8,12 +8,9 @@
  * Copyright (c) 2020, Adam Young.
  * All rights reserved.
  *
- * This file is part of memcached-win32.
- *
  * The applications are free software: you can redistribute it
  * and/or modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
+ * published by the Free Software Foundation, version 3.
  *
  * Redistributions of source code must retain the above copyright
  * notice, and must be distributed with the license document above.
@@ -26,6 +23,7 @@
  * This project is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * license for more details.
  * ==end==
  */
 
