874c6d8074185704f2278796daca0b60ce9f055d
adamyg <adamyg@gmail.com>
2025-01-22 02:31:42 +0800

    #4

--- libevent/evconfig-private.h
+++ libevent/evconfig-private.h
@@ -1,6 +1,17 @@
-#if !defined(EVENT_EVCONFIG__PRIVATE_H_) && !defined(__MINGW32__)
+#if !defined(EVENT_EVCONFIG__PRIVATE_H_)
 #define EVENT_EVCONFIG__PRIVATE_H_
 
-/* Nothing to see here.  Move along. */
+/* Enable GNU extensions on systems that have them.  */
+#ifndef _GNU_SOURCE
+#undef _GNU_SOURCE
+#endif
+
+/* Compiler tweaks */
+#if defined(__GNUC__)
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wint-to-pointer-cast"
+#pragma GCC diagnostic ignored "-Wcast-function-type"
+#pragma GCC diagnostic ignored "-Wshadow"
+#endif
 
 #endif
